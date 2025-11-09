34047cbebad71d9bf62db1ddf62b5114b7e7ea16
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2025-11-09 14:33:50 +0000

    windeployqt: add "INCLUDE_PDB"

--- deploy/call_windeployqt.ps1
+++ deploy/call_windeployqt.ps1
@@ -5,6 +5,7 @@ param (
     , [Parameter(Mandatory = $true)][string]${LIB_DIR_PATH}
     , [Parameter(Mandatory = $true)][string]${TARGET}
     , [Parameter(Mandatory = $true)][string]${INSTALL_ROOT}
+    , [Parameter(Mandatory = $true)][switch]${INCLUDE_PDB}
 
     , [Parameter(Mandatory = $true)][string]${DEPLOY_DIR_PATH}
 )
@@ -19,6 +20,11 @@ $PSBoundParameters | Format-List
 
 ${exeFilePath} = `
     Join-Path -Path ${INSTALL_ROOT} -ChildPath "${TARGET}.exe"
+
+if (${INCLUDE_PDB}) {
+    ${pdbFilePath} = `
+        Join-Path -Path ${INSTALL_ROOT} -ChildPath "${TARGET}.pdb"
+}
 
 ${innoSetupRequiredFilePaths} = @(
     , 'Beslyric.ico'
@@ -55,6 +61,10 @@ New-Item -Path ${DEPLOY_DIR_PATH} -ItemType Directory -Force
 
 
 Copy-Item -Path ${exeFilePath} -Destination ${DEPLOY_DIR_PATH} -PassThru
+
+if (${INCLUDE_PDB}) {
+    Copy-Item -Path ${pdbFilePath} -Destination ${DEPLOY_DIR_PATH} -PassThru
+}
 
 Copy-Item -Path ${innoSetupRequiredFilePaths} -Destination ${DEPLOY_DIR_PATH} -PassThru
 
